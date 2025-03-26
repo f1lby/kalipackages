eee5c528be2ee3a1ce9d3e4cda263a0cc741e0d0
f1lby <46318356+f1lby@users.noreply.github.com>
2025-03-26 12:40:49 +0000

    Create kaliapp.sh

---
+++ kaliapp.sh
@@ -0,0 +1,84 @@
+sudo apt-get clean
+sudo apt update && sudo apt upgrade
+sudo apt-get dist-upgrade
+
+sudo apt-get install –y aha
+sudo apt-get install –y amap
+sudo apt-get install –y bluetooth bluez bluez-tools rfkill
+sudo systemctl start bluetooth.service
+sudo systemctl enable bluetooth.service
+sudo apt-get install –y braa
+sudo apt-get install –y cheese
+sudo apt-get install –y cifs-utils
+sudo apt-get install –y chkrootkit
+sudo apt-get install –y dnsutils
+sudo apt-get install -y docker.io
+sudo apt-get install -y droopy
+sudo apt-get install -y dsniff
+sudo apt-get install -y enum4linux-ng
+sudo apt-get install -y feroxbuster
+sudo apt-get install -y gedit
+sudo apt-get install -y grc
+sudo apt-get install -y gobuster
+sudo apt-get install -y grepcidr
+sudo apt-get install -y hcxdumptool
+sudo apt-get install -y hcxtools
+sudo apt-get install -y htop
+sudo apt-get install -y ipcalc
+sudo apt-get install -y irpas
+sudo apt-get install -y jq
+sudo apt-get install -y jython
+sudo apt-get install -y kali-autopilot
+sudo apt-get install -y leafpad
+sudo apt-get install -y libpcap-dev
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libguestfs-tools
+sudo apt-get install -y libreoffice
+sudo apt-get install -y lynis
+sudo apt-get install -y lshw
+sudo apt-get install -y miniupnpc
+sudo apt-get install -y mtr
+sudo apt-get install -y multipath-tools
+sudo apt-get install -y ncat
+sudo apt-get install -y npm
+sudo apt-get install -y ntp
+sudo apt-get install -y open-iscsi
+sudo apt-get install -y oscanner
+sudo apt-get install -y p7zip-full
+sudo apt-get install -y pipx
+sudo apt-get install -y p0f
+sudo apt-get install -y putty
+sudo apt-get install -y putty-tools
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y remmina
+sudo apt-get install -y ridenum
+sudo apt-get install -y rkhunter
+sudo apt-get install -y routersploit
+sudo apt-get install -y rsh-client
+sudo apt-get install -y scite
+sudo apt-get install -y sctpscan
+sudo apt-get install -y seahorse
+sudo apt-get install -y seclists
+sudo apt-get install -y sipvicious
+sudo apt-get install -y slowhttptest
+sudo apt-get install -y snapd
+sudo systemctl enable --now snapd apparmor
+sudo apt-get install -y snmp-mibs-downloader;sudo download-mibs
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y ssh-audit
+sudo apt-get install -y sshuttle
+sudo apt-get install -y t50
+sudo apt-get install -y telnet
+sudo apt-get install -y testssl.sh
+sudo apt-get install -y tftp-hpa
+sudo apt-get install -y thunderbird
+sudo apt-get install -y tnscmd10g
+sudo apt-get install -y unicornscan
+sudo apt-get install -y wkhtmltopdf
+sudo apt-get install -y wmctrl
+sudo apt-get install -y xrdp
+sudo apt-get install -y yersinia
+sudo apt-get install -y wine32:i386
+sudo apt-get install -y wine
+sudo apt-get install -y tightvncserver
